f4fd1b5416b8a10b92217be0e6a9558435dd8156
lmmcdev <suscription@clmmail.com>
2025-08-07 15:39:41 -0400

    Update azureAuth.js

--- src/utils/azureAuth.js
+++ src/utils/azureAuth.js
@@ -3,7 +3,7 @@ export const msalConfig = {
     auth: {
       clientId: "08e5a940-4349-45b0-94ce-46505e0a99a3", // 👈 desde Azure Portal
       authority: "https://login.microsoftonline.com/7313ad10-b885-4b50-9c75-9dbbd975618f",
-      redirectUri: "https://witty-sky-076480f0f.2.azurestaticapps.net"
+      redirectUri: "https://witty-sky-076480f0f.2.azurestaticapps.net/"
       //redirectUri: "https://proud-tree-09d93ae0f.6.azurestaticapps.net/", // o tu dominio en Azure
       //redirectUri: "http://localhost:3000",
     },
@@ -16,4 +16,3 @@ export const msalConfig = {
   export const loginRequest = {
     scopes: ["User.Read", "User.ReadBasic.All"], // puedes agregar más scopes según tu app
   };
-  
